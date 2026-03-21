88ae174ecd1d2293d004796f0b160e60b4f7afcf
SBDJAY <sbdjay183@gmail.com>
2026-03-20 22:27:17 -0400

    Initial commit

---
+++ src/main/java/com/example/taskmanagerdanielpius/TaskManagerDanielPiusApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagerdanielpius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerDanielPiusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerDanielPiusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taskmanagerdanielpius/TaskManagerDanielPiusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagerdanielpius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerDanielPiusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
