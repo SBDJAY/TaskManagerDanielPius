0d77c62cb18adba914c3d7e8047672c793939645
SBDJAY <sbdjay183@gmail.com>
2026-03-21 17:43:08 -0400

    TaskModel Class Added

---
+++ src/main/java/com/example/taskmanagerdanielpius/model/TaskModel.java
@@ -0,0 +1,26 @@
+package com.example.taskmanagerdanielpius.model;
+
+public class TaskModel {
+    private Integer id;
+    private String title;
+    private String description;
+    private Boolean completed;
+    private String ownerUsername;
+
+
+    public TaskModel(Integer id, String title, String description, Boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+        this.ownerUsername = ownerUsername;
+    }
+
+    public Integer getId() { return id; }
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+    public Boolean isCompleted() { return completed; }
+    public String getOwnerUsername() { return ownerUsername; }
+
+    public void setCompleted(Boolean completed) { this.completed = completed; }
+}
